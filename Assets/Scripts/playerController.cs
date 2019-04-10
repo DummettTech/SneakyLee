14f10f99e14575f16065632152b5ec65c380ad3a
Sorrowsouls <alexdummett@hotmail.com>
2019-04-10 12:34:39 +0100

    Initial Commit
    Chucking everything up.
    Going to be working on AI complexity next, setting vision then working on patrols. Also updating .gitignore to work better with unity

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour {
+
+	void OnCollisionEnter(Collision col)
+	{
+        
+		if (col.gameObject.tag == "Guard") 
+		{
+			// Game Over
+			RestartGame();
+		}
+        Debug.Log(col.gameObject.tag);
+		if (col.gameObject.tag == "Exit") 
+		{
+            // Next Level / WIN!
+            Debug.Log("You Win!");
+            Text gameCompleteText = GameObject.FindGameObjectWithTag("CompleteText").GetComponent<Text>();
+            gameCompleteText.color = Color.yellow;
+            Debug.Log(gameCompleteText.text);
+            Time.timeScale = 0;
+        }
+	}
+
+	void Update()
+	{
+		if (Input.GetKey ("escape")) 
+		{
+			Application.Quit ();
+		}
+        if (Input.GetKey("r"))
+        {
+            RestartGame();
+        }
+    }
+
+	void RestartGame()
+	{
+		Scene currentScene = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene (currentScene.name);
+        Time.timeScale = 1;
+    }
+}
