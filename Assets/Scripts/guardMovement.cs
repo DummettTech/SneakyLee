14ce723a8c3e93a53e262c3585fa217475b8ba4c
Sorrowsouls <alexdummett@hotmail.com>
2019-04-10 12:34:39 +0100

    Initial Commit
    Chucking everything up.
    Going to be working on AI complexity next, setting vision then working on patrols. Also updating .gitignore to work better with unity

---
+++ Assets/Scripts/guardMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class guardMovement : MonoBehaviour {
+
+	public float speed = 6.0f;
+	GameObject playerObject;
+	UnityEngine.AI.NavMeshAgent agent;
+
+	void Start () {
+		playerObject = GameObject.FindGameObjectWithTag ("Player");
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+	}
+	
+
+	void Update () {
+		persuePlayer ();
+	}
+
+	void persuePlayer() {
+		agent.destination = playerObject.transform.position;
+	}
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private Rigidbody playerRigidbody;
+
+	void Start () {
+		playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+
+	void Update () {
+		move (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+	}
+
+	// TODO: Add gravity, verticle solutions to levels would be fun
+	void move(float i, float j) {
+		moveDirection.Set (i, 0.0f, j);
+		//moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection = moveDirection.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (moveDirection + transform.position);
+
+        if (i != 0 || j !=0)
+        {
+            transform.forward = Vector3.Normalize(moveDirection);
+        }
+
+    }
+}
