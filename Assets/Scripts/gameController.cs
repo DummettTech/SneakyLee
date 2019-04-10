656ee80696a8021a51eed43f8ea1852556307e80
Sorrowsouls <alexdummett@hotmail.com>
2019-04-10 17:36:21 +0100

    On another Level
    Added functionaility for Guards to follow sepperate paths from each other.
    Also added a new level for this.
    
    Will look to add "keys" preventing player from just leaving the level until they've collected all in the level

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
+public class gameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	void OnCollisionEnter(Collision col)
+	{
+        string[] levelNames = {"level_1", "level_2" };
+        int currentLevel = 0;
 
+		if (col.gameObject.tag == "Guard") 
+		{
+			// Game Over
+			RestartGame();
+		}
+
+		if (col.gameObject.tag == "Exit") 
+		{
+            // Next Level / WIN!
+            currentLevel = currentLevel + 1;
+
+            if (currentLevel != levelNames.Length)
+            {
+                SceneManager.LoadScene(levelNames[currentLevel]);
+            }
+
+            // Text gameCompleteText = GameObject.FindGameObjectWithTag("CompleteText").GetComponent<Text>();
+            // gameCompleteText.color = Color.yellow;
+            // Debug.Log(gameCompleteText.text);
+            // Time.timeScale = 0;
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	void Update()
+	{
+		if (Input.GetKey ("escape")) 
+		{
+			Application.Quit ();
+		}
+        if (Input.GetKey("r"))
+        {
+            RestartGame();
+        }
+    }
+
+	void RestartGame()
+	{
+		Scene currentScene = SceneManager.GetActiveScene ();
+		SceneManager.LoadScene (currentScene.name);
+        Time.timeScale = 1;
+    }
 }
