38125b06acb365c29f2cb7c33e430d072c157e73
Sorrowsouls <alexdummett@hotmail.com>
2019-04-10 15:33:29 +0100

    Smarter Guards
    Guards are now able to follow a route by placing waypoints, if the player hits the "bar" of vision and raycast hits player, the guards will stop following waypoints and go to players position.
    
    Known Problems - Guards a bit slow on rotation in this small map, and don't play well together in tight spaces.
    
    Will be looking to add a better cone of vision to guards, and more quality of life improvements on the waypoints

--- Assets/Scripts/guardMovement.cs
+++ Assets/Scripts/guardMovement.cs
@@ -1,23 +1,70 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 using System.Collections;
 
 public class guardMovement : MonoBehaviour {
 
 	public float speed = 6.0f;
-	GameObject playerObject;
-	UnityEngine.AI.NavMeshAgent agent;
+    float persueSpeed;
+    GameObject playerObject;
+    GameObject[] waypoints;
+    NavMeshAgent agent;
+    bool foundPlayer = false;
 
-	void Start () {
+    int currentWaypoint = 0;
+
+    void Start () {
 		playerObject = GameObject.FindGameObjectWithTag ("Player");
-		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		agent = GetComponent<NavMeshAgent> ();
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        persueSpeed = agent.speed + 2.0f;
 	}
 	
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            NavMeshHit hit;
+            agent.Raycast(playerObject.transform.position, out hit);
+            if (!hit.hit)
+            {
+                foundPlayer = true;
+            }
+        }
+    }
 
+    
 	void Update () {
-		persuePlayer ();
+
+        if (foundPlayer)
+        {
+            persuePlayer();
+        } else
+        {
+            if(!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                patrol();
+            }
+        }
+     
 	}
+
+    void patrol ()
+    {
+        if (currentWaypoint >= waypoints.Length) currentWaypoint = 0;
+
+        agent.destination = waypoints[currentWaypoint].transform.position;
+        currentWaypoint = currentWaypoint + 1;
+ 
+    }
 
 	void persuePlayer() {
+        agent.speed = persueSpeed;
+
+        // depending on how it plays, might want to change speed when persuing
 		agent.destination = playerObject.transform.position;
 	}
+
+
 }
